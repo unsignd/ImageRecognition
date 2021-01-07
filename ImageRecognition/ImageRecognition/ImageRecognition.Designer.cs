27cae63466d4689b27f343b30b7c4a5f17573921
ERRrOR404 <error99998@gmail.com>
2021-01-07 14:02:49 +0900

    Upload SourceCode

---
+++ ImageRecognition/ImageRecognition/ImageRecognition.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace ImageRecognition
+{
+    partial class ImageRecognition
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImageRecognition));
+            this.outPut = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // outPut
+            // 
+            this.outPut.Location = new System.Drawing.Point(12, 104);
+            this.outPut.Multiline = true;
+            this.outPut.Name = "outPut";
+            this.outPut.ReadOnly = true;
+            this.outPut.Size = new System.Drawing.Size(776, 334);
+            this.outPut.TabIndex = 0;
+            this.outPut.TextChanged += new System.EventHandler(this.outPut_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 53);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "New Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ImageRecognition
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.outPut);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ImageRecognition";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox outPut;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ImageRecognition/ImageRecognition/ImageRecognition.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Forms;
+using Patagames.Ocr;
+using System.Drawing;
+
+namespace ImageRecognition
+{
+    public partial class ImageRecognition : Form
+    {
+        public ImageRecognition()
+        {
+            InitializeComponent();
+        }
+
+        private void outPut_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string openstrFilename;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            Image image;
+            Bitmap bitmap = null;
+
+            openFileDialog.Title = "이미지 읽기";
+            openFileDialog.Filter = "All Files(*.*)|*.*|Bitmap File(*.bmp)|*.bmp|JPEG File(*.jpg)|*.jpg";
+
+            // 다이얼로그의 확인 처리
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                openstrFilename = openFileDialog.FileName;
+                image = Image.FromFile(openstrFilename);
+                bitmap = new Bitmap(image);
+            }
+            else
+            {
+                return;
+            }
+            outPut.Text = onTesseract(bitmap);
+        }
+
+        private string onTesseract(Bitmap bitmap)
+        {
+            try
+            {
+                using (var objOcr = OcrApi.Create())
+                {
+                    objOcr.Init(Patagames.Ocr.Enums.Languages.English);
+
+                    string output = objOcr.GetTextFromImage(bitmap);
+                    return output;
+                }
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+        }
+    }
+}
